3a53d0b25ed9be86eecda7ad951db5c14812e9b0
Graham Keenan <graham.keenan@outlook.com>
2025-12-12 18:58:26 +0000

    day 11 part 2: starting shortly

--- src/solutions/day11.rs
+++ src/solutions/day11.rs
@@ -1,7 +1,7 @@
 use super::Solve;
-use std::collections::{HashMap, VecDeque};
+use std::collections::{HashMap, HashSet, VecDeque};
 
-const DEBUG: bool = false;
+const DEBUG: bool = true;
 
 #[derive(Default)]
 pub struct Solution {
@@ -59,5 +59,48 @@ impl Solve for Solution {
         println!("Total paths: {paths}");
     }
 
-    fn part2(&mut self) {}
+    fn part2(&mut self) {
+        let mut paths = 0;
+        let map = debug_pt2();
+        let mut queue = VecDeque::new();
+        queue.push_back("svr".to_string());
+
+        let target = "out".to_string();
+        let chk1 = "fft".to_string();
+        let chk2 = "dac".to_string();
+
+        println!("Total paths: {paths}");
+    }
+}
+
+fn debug_pt2() -> HashMap<String, Vec<String>> {
+    let mut hm: HashMap<String, Vec<String>> = HashMap::new();
+    let input = r#"svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out
+"#;
+    let _: Vec<_> = input
+        .trim()
+        .split('\n')
+        .map(|line| {
+            let (node, neighbours) = line
+                .split_once(": ")
+                .expect("guaranteed from problem input");
+
+            let neighbours = neighbours.split(" ").map(|s| s.to_string()).collect();
+            hm.insert(node.to_string(), neighbours);
+        })
+        .collect();
+
+    hm
 }
