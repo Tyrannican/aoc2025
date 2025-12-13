9c0a746fa285fac7084e3e125bbf6b65ca58faea
Graham Keenan <graham.keenan@outlook.com>
2025-12-13 16:28:02 +0000

    day 11 part 2 - i hate memoisation

--- src/solutions/day11.rs
+++ src/solutions/day11.rs
@@ -1,11 +1,34 @@
 use super::Solve;
 use std::collections::{HashMap, HashSet, VecDeque};
 
-const DEBUG: bool = true;
+#[derive(Clone, Copy, Hash, Eq)]
+struct StrVec {
+    key: [u8; 3],
+}
+
+impl StrVec {
+    fn new(s: &[u8]) -> Self {
+        let mut key = [0; 3];
+        key[..s.len()].copy_from_slice(s);
+        Self { key }
+    }
+}
+
+impl AsRef<[u8]> for StrVec {
+    fn as_ref(&self) -> &[u8] {
+        &self.key
+    }
+}
+
+impl PartialEq for StrVec {
+    fn eq(&self, other: &Self) -> bool {
+        self.key.as_ref() == other.key.as_ref()
+    }
+}
 
 #[derive(Default)]
 pub struct Solution {
-    devices: HashMap<String, Vec<String>>,
+    devices: HashMap<StrVec, HashSet<StrVec>>,
 }
 
 impl Solution {
@@ -14,17 +37,48 @@ impl Solution {
         s.parse_input();
         s
     }
+
+    fn valid_paths(
+        &self,
+        curr: &StrVec,
+        dst: &StrVec,
+        visited: &mut HashSet<StrVec>,
+        seen_fft: bool,
+        seen_dac: bool,
+        cache: &mut HashMap<(StrVec, bool, bool), usize>,
+    ) -> usize {
+        if curr == dst {
+            return if seen_fft && seen_dac { 1 } else { 0 };
+        }
+
+        let key = (*curr, seen_fft, seen_dac);
+        if let Some(&cached) = cache.get(&key) {
+            return cached;
+        }
+
+        let mut count = 0;
+        let sfft = seen_fft || *curr == StrVec::new(b"fft");
+        let sdac = seen_dac || *curr == StrVec::new(b"dac");
+
+        if let Some(neighbours) = self.devices.get(curr) {
+            for neighbour in neighbours.iter() {
+                if !visited.contains(neighbour) {
+                    visited.insert(*neighbour);
+                    count += self.valid_paths(neighbour, dst, visited, sfft, sdac, cache);
+                    visited.remove(neighbour);
+                }
+            }
+        }
+
+        cache.insert(key, count);
+
+        count
+    }
 }
 
 impl Solve for Solution {
     fn parse_input(&mut self) {
-        let path = if DEBUG {
-            "./inputs/day11_debug.txt"
-        } else {
-            "./inputs/day11.txt"
-        };
-
-        let content = std::fs::read_to_string(path).expect("it exists");
+        let content = std::fs::read_to_string("./inputs/day11.txt").expect("it exists");
         let _: Vec<_> = content
             .trim()
             .split('\n')
@@ -33,8 +87,13 @@ impl Solve for Solution {
                     .split_once(": ")
                     .expect("guaranteed from problem input");
 
-                let neighbours = neighbours.split(" ").map(|s| s.to_string()).collect();
-                self.devices.insert(node.to_string(), neighbours);
+                let neighbours = neighbours
+                    .split(" ")
+                    .map(|s| StrVec::new(s.as_bytes()))
+                    .collect();
+
+                self.devices
+                    .insert(StrVec::new(node.as_bytes()), neighbours);
             })
             .collect();
     }
@@ -42,8 +101,8 @@ impl Solve for Solution {
     fn part1(&mut self) {
         let mut paths = 0;
         let mut queue = VecDeque::new();
-        queue.push_back("you".to_string());
-        let target = "out".to_string();
+        queue.push_back(StrVec::new("you".as_bytes()));
+        let target = StrVec::new("out".as_bytes());
 
         while let Some(curr) = queue.pop_front() {
             let neighbours = self.devices.get(&curr).unwrap();
@@ -51,56 +110,20 @@ impl Solve for Solution {
                 paths += 1;
             } else {
                 for neighbour in neighbours.iter() {
-                    queue.push_front(neighbour.clone());
+                    queue.push_front(*neighbour);
                 }
             }
         }
 
-        println!("Total paths: {paths}");
+        println!("Part 1: {paths}");
     }
 
     fn part2(&mut self) {
-        let mut paths = 0;
-        let map = debug_pt2();
-        let mut queue = VecDeque::new();
-        queue.push_back("svr".to_string());
-
-        let target = "out".to_string();
-        let chk1 = "fft".to_string();
-        let chk2 = "dac".to_string();
-
-        println!("Total paths: {paths}");
+        let src = StrVec::new(b"svr");
+        let dst = StrVec::new(b"out");
+        let mut visited = HashSet::new();
+        let mut cache = HashMap::new();
+        let count = self.valid_paths(&src, &dst, &mut visited, false, false, &mut cache);
+        println!("Part 2: {count}");
     }
-}
-
-fn debug_pt2() -> HashMap<String, Vec<String>> {
-    let mut hm: HashMap<String, Vec<String>> = HashMap::new();
-    let input = r#"svr: aaa bbb
-aaa: fft
-fft: ccc
-bbb: tty
-tty: ccc
-ccc: ddd eee
-ddd: hub
-hub: fff
-eee: dac
-dac: fff
-fff: ggg hhh
-ggg: out
-hhh: out
-"#;
-    let _: Vec<_> = input
-        .trim()
-        .split('\n')
-        .map(|line| {
-            let (node, neighbours) = line
-                .split_once(": ")
-                .expect("guaranteed from problem input");
-
-            let neighbours = neighbours.split(" ").map(|s| s.to_string()).collect();
-            hm.insert(node.to_string(), neighbours);
-        })
-        .collect();
-
-    hm
 }
